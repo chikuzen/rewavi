1d4d7c49eabe9a249209302720d33c510057110e
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2018-01-07 15:42:04 +0100

    Make rewavi_log inline

--- src/common.h
+++ src/common.h
@@ -23,4 +23,21 @@ typedef enum {
     LOG_WARNING
 } log_t;
 
-int rewavi_log(const char *name, log_t level, const char *message, ...);
+inline int rewavi_log(const char* name, log_t level, const char *message, ...)
+{
+    char *prefix;
+    va_list args;
+    va_start(args, message);
+    switch (level)
+    {
+    case LOG_INFO:    prefix = "info";    break;
+    case LOG_WARNING: prefix = "warn";    break;
+    case LOG_ERROR:   prefix = "error";   break;
+    default:          prefix = "unknown"; break;
+    }
+    fprintf(stderr, "%s [%s]: ", name, prefix);
+    vfprintf(stderr, message, args);
+    va_end(args);
+    return level;
+}
+
