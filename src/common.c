047a6c389ee65a3a0f5defd055343e00f5e77d11
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2018-01-07 15:40:25 +0100

    Formatted code

--- src/common.c
+++ src/common.c
@@ -4,28 +4,16 @@ int rewavi_log(const char* name, log_t level, const char *message, ...)
 {
     char *prefix;
     va_list args;
-
     va_start(args, message);
-
-    switch(level)
+    switch (level)
     {
-        case LOG_INFO:
-            prefix = "info";
-            break;
-        case LOG_WARNING:
-            prefix = "warn";
-            break;
-        case LOG_ERROR:
-            prefix = "error";
-            break;
-        default:
-            prefix = "unknown";
-            break;
+    case LOG_INFO:    prefix = "info";    break;
+    case LOG_WARNING: prefix = "warn";    break;
+    case LOG_ERROR:   prefix = "error";   break;
+    default:          prefix = "unknown"; break;
     }
-
     fprintf(stderr, "%s [%s]: ", name, prefix);
     vfprintf(stderr, message, args);
-
     va_end(args);
     return level;
 }
