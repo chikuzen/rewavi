a315a39277c22a725fcccef871e5a3da5e487008
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2018-01-08 19:28:18 +0100

    Removed rewavi_log

--- src/common.h
+++ src/common.h
@@ -16,27 +16,3 @@
 #endif
 
 #include "version.h"
-
-typedef enum {
-    LOG_INFO,
-    LOG_ERROR,
-    LOG_WARNING
-} log_t;
-
-inline int rewavi_log(const char* name, log_t level, const char *message, ...)
-{
-    char *prefix;
-    va_list args;
-    va_start(args, message);
-    switch (level)
-    {
-    case LOG_INFO:    prefix = "info";    break;
-    case LOG_WARNING: prefix = "warn";    break;
-    case LOG_ERROR:   prefix = "error";   break;
-    default:          prefix = "unknown"; break;
-    }
-    fprintf(stderr, "%s [%s]: ", name, prefix);
-    vfprintf(stderr, message, args);
-    va_end(args);
-    return level;
-}
