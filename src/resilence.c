92a9ff2ccb46f06eb8cdcf6b81fa2123b47921a7
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2018-01-08 19:28:05 +0100

    Removed macros

--- src/resilence.c
+++ src/resilence.c
@@ -1,11 +1,5 @@
 #include "common.h"
 
-#define PRINT_LOG(level, ...) \
-    rewavi_log("resilence", level, __VA_ARGS__)
-
-#define CLOSE_IF_ERR(cond, ...) \
-    if (cond) {retcode = PRINT_LOG(LOG_ERROR, __VA_ARGS__); goto close;}
-
 #define DEFAULT_LENGTH 1.0
 #define DEFAULT_CHANNELS 2
 #define DEFAULT_SAMPLERATE 48000
@@ -18,14 +12,19 @@ static void usage(void);
 int main (int argc, char **argv)
 {
     if (argc < 2)
+    {
         usage();
+        return 1;
+    }
 
-    int retcode = 0;
     FILE *output_fh = NULL;
 
-    CLOSE_IF_ERR(!strcmp(argv[1], "-f") || !strcmp(argv[1], "-l") ||
-                 !strcmp(argv[1], "-c") || !strcmp(argv[1], "-b"),
-                 "Missing output target.\n");
+    if (!strcmp(argv[1], "-f") || !strcmp(argv[1], "-l") ||
+        !strcmp(argv[1], "-c") || !strcmp(argv[1], "-b")) 
+    { 
+        fprintf(stderr, "Missing output target.\n");
+        return 1;
+    }
 
     WAVEFORMATEX wavefmt = { WAVE_FORMAT_PCM, DEFAULT_CHANNELS, DEFAULT_SAMPLERATE,
                              NO_DEFAULT, NO_DEFAULT, DEFAULT_SAMPLEBITS, 0 };
@@ -34,7 +33,11 @@ int main (int argc, char **argv)
 
     int i = 1;
     while (argv[++i]) {
-        CLOSE_IF_ERR(argv[i][0] != '-', "Invarid argument '%s'\n", argv[i]);
+        if (argv[i][0] != '-') 
+        { 
+            fprintf(stderr, "Invarid argument '%s'\n", argv[i]);
+            return 1;
+        }
         switch (argv[i][1]) {
         case 'f':
             wavefmt.wFormatTag = WAVE_FORMAT_IEEE_FLOAT;
@@ -42,41 +45,55 @@ int main (int argc, char **argv)
             break;
         case 'l':
             length = atof(argv[++i]);
-            CLOSE_IF_ERR(length <= 0, "Invalid length given after -l\n");
+            if (length <= 0) 
+            { 
+                fprintf(stderr, "Invalid length given after -l\n");
+                return 1;
+            }
             break;
         case 'c':
             wavefmt.nChannels = atoi(argv[++i]);
-            CLOSE_IF_ERR(!wavefmt.nChannels || wavefmt.nChannels > 32,
-                         "Invalid number of channels '%s' given after -c\n", argv[i]);
+            if (!wavefmt.nChannels || wavefmt.nChannels > 32) 
+            { 
+                fprintf(stderr, "Invalid number of channels '%s' given after -c\n", argv[i]);
+                return 1;
+            }
             break;
         case 's':
             wavefmt.nSamplesPerSec = atoi(argv[++i]);
-            CLOSE_IF_ERR(!wavefmt.nSamplesPerSec || wavefmt.nSamplesPerSec > 192000,
-                         "Invalid sample rate '%s' given after -s\n", argv[i]);
+            if (!wavefmt.nSamplesPerSec || wavefmt.nSamplesPerSec > 192000) 
+            { 
+                fprintf(stderr, "Invalid sample rate '%s' given after -s\n", argv[i]);
+                return 1;
+            }
             break;
         case 'b':
             wavefmt.wBitsPerSample = atoi(argv[++i]);
-            CLOSE_IF_ERR(!wavefmt.wBitsPerSample ||
-                         wavefmt.wBitsPerSample % 8 ||
-                         wavefmt.wBitsPerSample > 32,
-                         "Invalid bits per sample '%s' given after -b\n", argv[i]);
+            if (!wavefmt.wBitsPerSample || wavefmt.wBitsPerSample % 8 || wavefmt.wBitsPerSample > 32) 
+            { 
+                fprintf(stderr, "Invalid bits per sample '%s' given after -b\n", argv[i]);
+                return 1;
+            }
             samplebits_given = 1;
             break;
         default:
-            PRINT_LOG((retcode = LOG_ERROR), "Invarid argument '%s'\n", argv[i]);
-            goto close;
+            fprintf(stderr, "Invarid argument '%s'\n", argv[i]);
+            return 1;
         }
     }
 
-    CLOSE_IF_ERR(wavefmt.wFormatTag + samplebits_given > 3,
-                 "'-f' and '-b' cannot be used at the same time.\n");
+    if (wavefmt.wFormatTag + samplebits_given > 3) 
+    { 
+        fprintf(stderr, "'-f' and '-b' cannot be used at the same time.\n");
+        return 1;
+    }
 
     wavefmt.nBlockAlign = wavefmt.nChannels * wavefmt.wBitsPerSample >> 3;
     wavefmt.nAvgBytesPerSec = wavefmt.nBlockAlign * wavefmt.nSamplesPerSec;
     uint64_t filesize = (uint64_t)(wavefmt.nAvgBytesPerSec * length);
     uint32_t size_for_write = (uint32_t)filesize + 36;
     if (filesize > 0xFFFFFFFF - 36) {
-        PRINT_LOG(LOG_WARNING, "WAV file will be larger than 4 GB.\n");
+        fprintf(stderr, "WAV file will be larger than 4 GB.\n");
         size_for_write = wavefmt.nBlockAlign * ((0xFFFFFFFF - 36) / wavefmt.nBlockAlign) + 36;
     }
 
@@ -89,9 +106,13 @@ int main (int argc, char **argv)
     } else
         fopen_s(&output_fh, argv[1], "wb");
 
-    CLOSE_IF_ERR(!output_fh, "Fail to create/open file.\n");
+    if (!output_fh) 
+    { 
+        fprintf(stderr, "Fail to create/open file.\n");
+        return 1;
+    }
 
-    PRINT_LOG(LOG_INFO, "Writing silence WAV file %s ...\n", dupout ? "to stdout" : argv[1]);
+    fprintf(stderr, "Writing silence WAV file %s ...\n", dupout ? "to stdout" : argv[1]);
 
     fwrite("RIFF", 1, 4, output_fh);
     fwrite(&size_for_write, 4, 1, output_fh);
@@ -114,14 +135,18 @@ int main (int argc, char **argv)
     }
 
     fflush(output_fh);
+    fclose(output_fh);
 
-    CLOSE_IF_ERR(wrote != filesize, "Writing failed...\n");
-    PRINT_LOG(LOG_INFO, "WAV file written successfully.\n");
-
-close:
-    if (output_fh)
-        fclose(output_fh);
-    return retcode;
+    if (wrote != filesize)
+    {
+        fprintf(stderr, "Writing failed...\n");
+        return 1;
+    }
+    else
+    {
+        fprintf(stderr, "WAV file written successfully.\n");
+        return 0;
+    }
 }
 
 static void usage(void)
@@ -146,5 +171,4 @@ static void usage(void)
         "  -f : use 32bit floating-point samples instead of 8/16/24/32bit integer.\n"
         "       this option cannot be used with -b at the same time.\n",
         VERSION);
-    exit(1);
 }
