bda49a64cf0c0f4073f37b42548155acbc73f844
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2018-01-08 19:29:35 +0100

    Removed common.h

--- src/rewavi.c
+++ src/rewavi.c
@@ -1,4 +1,19 @@
-#include "common.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdarg.h>
+#include <io.h>
+#include <fcntl.h>
+
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+#include <vfw.h>
+
+#ifndef WAVE_FORMAT_IEEE_FLOAT
+#define WAVE_FORMAT_IEEE_FLOAT 3
+#endif
+
+#include "version.h"
 
 #pragma comment(lib, "vfw32.lib")
 
